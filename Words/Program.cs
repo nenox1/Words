81f5940a4f6a3d7805358054833fdae6970d5b60
Nenad Pantelic <npantelic@hotmail.com>
2021-07-15 12:54:01 +0500

    making changes in first branch

---
+++ Words/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input letter or string: ");
+            string letter = Console.ReadLine();
+
+            int counter = 0;
+            List<string> list = new List<string>();
+
+            string[] words =
+            {
+                "home", "programming", "victory", "C#", "football", "sport",
+                "book", "learn", "dream", "fun"
+            };
+
+            for(int i = 0;i < words.Length; i++){
+                if (words[i].Contains(letter)) { 
+                    counter++;
+                    list.Add(words[i]);
+                }                
+            }
+
+            if (counter!=0)
+            {                
+                foreach (var e in list)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No match");                
+            }
+        }
+    }
+}
